b4e538790a8a7314b2b4363e3bf53c8074e65c36
Arpita <arpitapatel3010@gmail.com>
2022-08-28 18:05:47 +0530

    Add files via upload

---
+++ Module4/5.py
@@ -0,0 +1,29 @@
+#Write a Python program to read last n lines of a file.
+
+
+
+
+
+
+
+
+
+
+#Write a Python program to read last n lines of a file.
+
+#input the text file
+InputFile = "file.txt"
+
+#Enter the N lines of file you want to see
+N  = int(input("Enter N values: "))
+
+#reading a file /file opening in a read mode
+file1 = open(InputFile,"r")
+line_list = file1.readlines()
+print(line_list)
+
+print("The",N,"lines are")
+
+#Travaersing the line list
+for txt in (line_list[N:]):
+    print(txt)
