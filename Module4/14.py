2cb17b27ce21169dd1622842c295e236694a4ff2
Arpita <arpitapatel3010@gmail.com>
2022-09-01 10:03:20 +0530

    Add files via upload

---
+++ Module4/14.py
@@ -0,0 +1,2 @@
+#When will the else part of try-except-else be executed?
+#ANS The else part of try-except-else be executed when no exception occurs
---
+++ Module4/15.py
@@ -0,0 +1,2 @@
+#Can one block of except statements handle multiple exception?
+#ANS  yes  like except [TypeError, SyntaxError] 
